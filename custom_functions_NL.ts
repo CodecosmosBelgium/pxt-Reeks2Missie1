dee91cbda43722bc6f97c4c47f92f063450a26dd
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2023-10-04 09:48:44 +0200

    Update custom_functions.ts, custom_functions_NL.ts

--- custom_functions_NL.ts
+++ custom_functions_NL.ts
@@ -24,12 +24,13 @@ namespace AgentExtensionNL {
     }
 
     //% block="agent beweeg $direction met $amount"
+    //% amount.defl=1
     export function agentMoveFourDirection(direction: FourDirection, amount: number) {
         for (let i = 0; i < amount; i++) {
             player.execute(`execute @p ~ ~ ~ setblock 87 43 74 air`)
             agent.move(direction, 1)
             let posBelowAgent = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
-            if (blocks.testForBlock(COBBLESTONE, posBelowAgent)) {
+            if (blocks.testForBlock(POINTED_DRIPSTONE, posBelowAgent)) {
                 wrongMoves++
                 player.execute(`execute @c ~ ~ ~ particle rwm:barrier ~ ~1 ~`)
             }
